c45e04c923aac41234f027431998aafba22e9de4
grahambollard1993 <131812868+grahambollard1993@users.noreply.github.com>
2023-08-28 12:03:15 +0000

    Update

---
+++ PhonesApi/Data/PhonesDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PhonesApi.Models;
+namespace PhonesApi.Data
+{
+public class PhonesDbContext : DbContext
+{
+    public PhonesDbContext(DbContextOptions<PhonesDbContext> options) :
+base(options)
+{}
+public DbSet<Phones> Movies { get; set; } = null!;
+}
+}
