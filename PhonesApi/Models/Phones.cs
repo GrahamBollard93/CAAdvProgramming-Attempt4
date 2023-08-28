2f4e53af0b63a77937a92afc15f45a1b68068dc8
GrahamBollard93 <10617126@mydbs.ie>
2023-08-28 18:18:16 +0100

    update

--- PhonesApi/Models/Phones.cs
+++ PhonesApi/Models/Phones.cs
@@ -7,7 +7,7 @@ public class Phones
     public string? Title { get; set; }
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
+  
     public decimal Price { get; set; }
     }
 }
